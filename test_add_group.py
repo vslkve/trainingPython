ca32c6665fc02805622a5105cbe1b3a37f3c3313
Ekaterina V <kateotm@gmail.com>
2023-09-19 15:22:40 +0300

    Задание 1

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -10,32 +12,35 @@ import unittest, time, re
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = WebDriver()
         self.wd.implicitly_wait(30)
 
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("fsdfsf")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dfsfs")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("fsdfs")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        wd.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys("fsdfsf")
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys("dfsfs")
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys("fsdfs")
+        wd.find_element(By.NAME, "submit").click()
+        wd.find_element(By.LINK_TEXT, "group page").click()
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def tearDown(self):
+        self.wd.quit()
 
     def is_element_present(self, how, what):
         try:
@@ -50,9 +55,6 @@ class TestAddGroup(unittest.TestCase):
         except NoAlertPresentException as e:
             return False
         return True
-
-    def tearDown(self):
-        self.wd.quit()
 
 
 if __name__ == "__main__":
