2e7c75dddeaa7346c4b3dbe9fa480c2fe286d22f
Ekaterina V <kateotm@gmail.com>
2023-10-08 22:14:42 +0300

    Проверка равенства объектов и списков

--- model/contact.py
+++ model/contact.py
@@ -14,3 +14,9 @@ class Contact:
         self.email = email
         self.id = id
 
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.lastname == other.lastname and self.firstname == other.firstname
+
--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -9,3 +9,5 @@ def test_delete_first_contact(app):
     app.contact.delete_first_contact()
     new_contact = app.contact.get_contact_list()
     assert len(old_contact) - 1 == len(new_contact)
+    old_contact[0:1] = []
+    assert old_contact == new_contact
