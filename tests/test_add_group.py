2f885c49b4ea7e8091d4fd5173a4429f7d2ef5d6
Ekaterina V <kateotm@gmail.com>
2023-10-18 22:35:53 +0300

    Параметризовать тесты для создания групп и контактов

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
+import pytest
+import random
+import string
+
 from model.group import Group
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group("", "", "")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 10))
+    for i in range(5)
+]
+
+# testdata = [
+#     Group(name=name, header=header, footer=footer)
+#     for name in ["", random_string("name", 10)]
+#     for header in ["", random_string("header", 20)]
+#     for footer in ["", random_string("footer", 20)]]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group("fsdfsf", "dfsfs", "fsdfs")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
