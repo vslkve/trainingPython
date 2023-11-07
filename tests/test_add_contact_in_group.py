d8a16db3933a337e541ce60441876ef231a12892
Ekaterina V <kateotm@gmail.com>
2023-11-07 18:40:38 +0300

    Добавлена проверка, что контакт отсуствует в выбранной группе

--- tests/test_add_contact_in_group.py
+++ tests/test_add_contact_in_group.py
@@ -4,19 +4,22 @@ import random
 
 
 def test_add_some_contact_in_group(app, orm):
-    # проверка, что существует контакт
-    if len(orm.get_contact_list()) == 0:
-        app.contact.create_contact(
-            Contact(firstname="Kat", middlename="Al", lastname="GV", nickname="vs", title="ascss",
-                    company="comp", address="Kzn", homephone="66675666", mobilephone="765756765", workphone="5675",
-                    secondaryphone="321123", email="e@m.com"))
     # проверка, что существует группа
     if len(orm.get_group_list()) == 0:
         app.group.create(Group("fsdfsf", "dfsfs", "fsdfs"))
-    # получаем списки
+
+    # получаем список групп
     groups = orm.get_group_list()
-    contacts = orm.get_contact_list()
     group = random.choice(groups)
+
+    # проверяем, существуют ли контакты, не принадлежащие выбранной группе
+    if len(orm.get_contacts_not_in_group(group)) == 0:
+        app.contact.create_contact(
+            Contact(firstname="Kat", middlename="Al", address="Kzn", homephone="66675666", email="e@m.com"))
+
+    # получаем список контактов, которые отсуствуют в выбранной группе
+    contacts = orm.get_contacts_not_in_group(group)
     contact = random.choice(contacts)
+
     app.contact.add_contact_to_group_by_id(contact.id, group.id)
     assert contact in orm.get_contacts_in_group(group)
